e06127a227f221e4452fe620867bc174e826e64e
student <student@PC-105.labmif>
2016-03-02 17:14:36 +0100

    Dodanie statystyki

--- Losowosc/src/com/dam/los.java
+++ Losowosc/src/com/dam/los.java
@@ -1,5 +1,6 @@
 package com.dam;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -20,16 +21,26 @@ public class los {
         */
         Generator generator = new Generator(MOD);
         
+        int[] statsX = new int[10];
+        int[] statsY = new int[10];
+        
         for (int i=0 ; i<1000 ; i++) {
             long[] coordinates = generator.getCoordinates();
                     
             float x = (float) coordinates[0] / MOD;
             float y = (float) coordinates[1] / MOD;
              
+            statsX[(int)(x*10)]++;
+            statsY[(int)(y*10)]++;
             System.out.println("Wygenerowałem punkt : (" + x + " | " + y + ")");
           /*
           * wypełnienie punktu (x|y) kolorem
            */
         }
+        
+        
+            System.out.println("Statystyka podpowiada: ");
+            System.out.println("Równość rozproszenie na osi X : " + Arrays.toString(statsX));
+            System.out.println("Równość rozproszenie na osi Y : " + Arrays.toString(statsY));
     }
 }

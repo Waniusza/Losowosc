644d2c6a97cfe961cb2aec72ceb5cb7898c39ecb
student <student@PC-105.labmif>
2016-03-02 17:00:22 +0100

    Zainicjalizowanie generatora liczb pseudolosowych

---
+++ Losowosc/src/com/dam/Generator.java
@@ -0,0 +1,31 @@
+package com.dam;
+
+
+/**
+ *
+ * @author waniusza
+ */
+public class Generator {
+
+    
+    private static long mod;
+    private static long value;
+    private static long increase = 214531366;
+    public Generator(long mod) {
+        this.mod = mod;
+        value = System.currentTimeMillis();
+    }
+    
+    
+    public static long[] getCoordinates() {
+        long[] res = new long[2];
+        res[0] = countValue();
+        res[1] = countValue();
+        return res;
+    }
+    
+    private static long countValue() {
+        value = ((18L * value + increase) * 3) % mod;
+        return value;
+    }
+}
---
+++ Losowosc/src/com/dam/los.java
@@ -0,0 +1,35 @@
+package com.dam;
+
+import java.util.List;
+
+/**
+ *
+ * @author waniusza && damian
+ */
+public class los {
+
+    
+    private static long MOD = (Long.MAX_VALUE/2);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        * zainicjalizowanie tabeli OXY 
+        */
+        Generator generator = new Generator(MOD);
+        
+        for (int i=0 ; i<1000 ; i++) {
+            long[] coordinates = generator.getCoordinates();
+                    
+            float x = (float) coordinates[0] / MOD;
+            float y = (float) coordinates[1] / MOD;
+             
+            System.out.println("Wygenerowałem punkt : (" + x + " | " + y + ")");
+          /*
+          * wypełnienie punktu (x|y) kolorem
+           */
+        }
+    }
+}

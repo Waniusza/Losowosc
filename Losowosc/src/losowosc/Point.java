584a3b24ac59e19e62f62cd765be6ce6a15744eb
Waniusza <janusz.sokolow@gmail.com>
2016-03-08 22:46:39 +0100

    Poprawiony wygląd
    Podpięcie szablonu z danymi
    
    Poprawienie algorytku generowania

--- Losowosc/src/losowosc/Point.java
+++ Losowosc/src/losowosc/Point.java
@@ -5,22 +5,25 @@
  */
 package losowosc;
 
-import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class Point extends JFrame {
-
-    public Point() {
-
+    
+    private int a,b,n;
+    
+    public Point(int a, int b, int n) {
+        this.a = a;
+        this.b = b;
+        this.n = n;
         initUI();
     }
 
     private void initUI() {
 
-        final Los surface = new Los();
+        final Los surface = new Los(a, b, n);
         add(surface);
 
         addWindowListener(new WindowAdapter() {
@@ -32,20 +35,20 @@ public class Point extends JFrame {
         });
 
         setTitle("Points");
-        setSize(350, 250);
+        setSize(250, 250);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args) {
-
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-
-                Point ex = new Point();
-                ex.setVisible(true);
-            }
-        });
-    }
+//    public static void main(String[] args) {
+//
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                Point ex = new Point();
+//                ex.setVisible(true);
+//            }
+//        });
+//    }
 }
